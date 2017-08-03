78acf752ea28c1d8a8d34745c91e3247b75554a6
Trevor Gipper <trevor.gipper@gmail.com>
2017-08-03 08:16:00 -0700

    start assignment-16

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,10 @@ posts = Post.all
     )
 end
 
+Post.find_or_create_by!(title: "Unique Title", body: "Unique Body")
+
+
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
