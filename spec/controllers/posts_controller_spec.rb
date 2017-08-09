5a6fe107e18464d51acfbf97cca8aa0fab88ebb7
Trevor Gipper <trevor.gipper@gmail.com>
2017-08-09 12:59:28 -0700

    complete checkpoint-17

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,13 +2,21 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
 
+  let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "assigns [my_post] to @posts" do
+      get :index
+      expect(assigns(:posts)).to eq([my_post])
+    end
   end
 
+=begin
   describe "GET #show" do
     it "returns http success" do
       get :show
@@ -29,5 +37,6 @@ RSpec.describe PostsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+=end
 
 end
